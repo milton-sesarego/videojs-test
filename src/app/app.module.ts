c7e109abd16c66c85f78788b0fb696dd5c26215f
milton-sesarego <milton.sesarego@gmail.com>
2020-08-10 00:36:07 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VjsPlayerComponent } from './vjs-player/vjs-player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VjsPlayerComponent
   ],
   imports: [
     BrowserModule,
