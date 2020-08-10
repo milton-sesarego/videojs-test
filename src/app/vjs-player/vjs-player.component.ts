a4331460eab423517cfffedf550d981c9b630a01
milton-sesarego <milton.sesarego@gmail.com>
2020-08-10 15:29:13 -0300

    poster

--- src/app/vjs-player/vjs-player.component.ts
+++ src/app/vjs-player/vjs-player.component.ts
@@ -1,20 +1,20 @@
 // vjs-player.component.ts
-import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit, AfterViewInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import videojs from 'video.js';
+import 'videojs-sprite-thumbnails';
 
 @Component({
-  selector: 'app-vjs-player',
+  selector: 'vjs-player',
   template: `
-    <video #target class="video-js" controls muted playsinline preload="none"></video>
+    <video #target class="video-js vjs-16-9" controls muted playsinline preload="none"></video>
   `,
   styleUrls: [
     './vjs-player.component.scss'
   ],
   encapsulation: ViewEncapsulation.None,
 })
-export class VjsPlayerComponent implements OnInit, OnDestroy {
+export class VjsPlayerComponent implements AfterViewInit, OnDestroy {
   @ViewChild('target', {static: true}) target: ElementRef;
-  // see options: https://github.com/videojs/video.js/blob/mastertutorial-options.html
   @Input() options: {
       fluid: boolean,
       aspectRatio: string,
@@ -24,17 +24,27 @@ export class VjsPlayerComponent implements OnInit, OnDestroy {
           type: string,
       }[],
   };
+
   player: videojs.Player;
 
   constructor(
     private elementRef: ElementRef,
   ) { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
     // instantiate Video.js
     this.player = videojs(this.target.nativeElement, this.options, function onPlayerReady() {
-      console.log('onPlayerReady', this);
+    // console.log('onPlayerReady', this);
     });
+
+    /*
+    this.player.spriteThumbnails({
+      interval: this.target.nativeElement.duration / 4,
+      url: '../assets/videos/oceans.jpeg',
+      width: 256,
+      height: 106
+    });
+    console.log(this.target.nativeElement.duration);*/
   }
 
   ngOnDestroy() {
