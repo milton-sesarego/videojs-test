d100c7cfdde70571dd16ca07eaa6b350699fe356
milton-sesarego <milton.sesarego@gmail.com>
2020-08-14 11:07:26 -0300

    audio con videojs-wavesurfer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,14 @@ import { VideoJsPlayerOptions } from 'video.js';
 })
 
 export class AppComponent implements OnInit{
-  opciones: Array<{config: VideoJsPlayerOptions, src: object, idx: number}>;
+  opciones: Array<VideoJsPlayerOptions>;
   constructor(){}
 
   ngOnInit(){
+    /*
+    * PARA VIDEOS USAR sources
+    * PARA AUDIOS USAR src
+    */
     this.opciones = [/*
       {
         poster: '../assets/videos/posters/oceans.jpeg',
@@ -26,52 +30,41 @@ export class AppComponent implements OnInit{
         preload: 'none',
         sources: [{ src: '../assets/videos/sample_1280x720.m4v', type: 'video/mp4' }]},*/
       {
-        config: {
-          controls: true,
-          aspectRatio: '4:3',
-          muted: true,
-          techOrder: ['youtube'],
-          sources: [{ src: 'https://www.youtube.com/watch?v=0FnPMIy42_4', type: 'video/youtube' }]
-        },
-        src: { src: 'https://www.youtube.com/watch?v=0FnPMIy42_4', type: 'video/youtube' },
-        idx: 0
+        controls: true,
+        aspectRatio: '4:3',
+        muted: true,
+        techOrder: ['youtube'],
+        sources: [{ src: 'https://www.youtube.com/watch?v=0FnPMIy42_4', type: 'video/youtube' }]
       },
       {
-        config: {
-          controls: true,
-          // aspectRatio: '4:3',
-          fluid: true,
-          muted: true,
-          techOrder: ['Vimeo'],
-          sources:  [{ src: 'https://vimeo.com/258591291', type: 'video/vimeo' }]
-        },
-        src: { src: 'https://vimeo.com/258591291', type: 'video/vimeo' },
-        idx: 1
+        controls: true,
+        // aspectRatio: '4:3',
+        fluid: true,
+        muted: true,
+        techOrder: ['Vimeo'],
+        sources:  [{ src: 'https://vimeo.com/258591291', type: 'video/vimeo' }]
       },
       {
-        config: {
-          controls: true,
-          autoplay: false,
-          fluid: false,
-          loop: false,
-          width: 600,
-          height: 300,
-          plugins: {
-            // configure videojs-wavesurfer plugin
-            wavesurfer: {
-                backend: 'MediaElement',
-                displayMilliseconds: true,
-                debug: true,
-                waveColor: 'gray',
-                progressColor: 'white',
-                cursorColor: 'white',
-                hideScrollbar: true
-            }
-          },
+        controls: true,
+        autoplay: false,
+        fluid: true,
+        loop: false,
+        width: 600,
+        height: 300,
+        plugins: {
+          // configure videojs-wavesurfer plugin
+          wavesurfer: {
+              backend: 'MediaElement',
+              displayMilliseconds: true,
+              //debug: true,
+              waveColor: 'gray',
+              progressColor: 'white',
+              cursorColor: 'white',
+              hideScrollbar: true
+          }
         },
-        src: { src: 'assets/1.mp3', type: 'audio/mp3' },
-        idx: 2
-      },
+        src: 'assets/1.mp3'
+      }
     ];
 
 
--- src/app/vjs-player/vjs-player.component.ts
+++ src/app/vjs-player/vjs-player.component.ts
@@ -1,11 +1,11 @@
 // vjs-player.component.ts
 import { Component, ElementRef, Input, OnDestroy, AfterViewInit, ViewEncapsulation } from '@angular/core';
 import videojs, { VideoJsPlayerOptions } from 'video.js';
-//import 'videojs-sprite-thumbnails';
 import 'videojs-youtube';
 import '@devmobiliza/videojs-vimeo/dist/videojs-vimeo.cjs';
 import * as WaveSurfer from 'wavesurfer.js';
 import * as Wavesurfer from 'videojs-wavesurfer/dist/videojs.wavesurfer.js';
+//import 'videojs-sprite-thumbnails';
 
 @Component({
   selector: 'vjs-player',
@@ -17,10 +17,8 @@ import * as Wavesurfer from 'videojs-wavesurfer/dist/videojs.wavesurfer.js';
 export class VjsPlayerComponent implements AfterViewInit, OnDestroy {
   private player;
   private plugin;
-  private config: VideoJsPlayerOptions;
-  idx;
-
-  @Input() options: {config: VideoJsPlayerOptions, src: object, idx: number};
+  @Input() idx: number;
+  @Input() options: VideoJsPlayerOptions;
 
   constructor(private elementRef: ElementRef) {
     this.player = false;
@@ -29,9 +27,7 @@ export class VjsPlayerComponent implements AfterViewInit, OnDestroy {
   }
 
   ngAfterViewInit() {
-
-    console.log(this.options.idx)
-    this.player = videojs(document.getElementById('target-' + this.options.idx), this.options.config, () => {
+    this.player = videojs(document.getElementById('target-' + this.idx), this.options, () => {
       this.player.src(this.options.src);
     });
 
